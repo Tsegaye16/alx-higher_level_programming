232336af073b1445d3c8b944f83a196926793872
Tsegaye16 <abewatsegaye16@gmail.com>
2023-06-06 14:27:20 -0700

    print hexadecimal

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for j in range(10):
-    for k in range(j+1, 10):
-        print("{}, {}".format(j, k), end="")
-print("".format(chr(8)), end="\n")
+for m in range(10):
+    for n in range(m + 1, 10):
+        print(
+            "{}{}".format(m, n),
+            end=", " if int(str(m) + str(n)) < 89 else "\n"
+            )
