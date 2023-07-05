0881aa8dc0291b6db74e8c5fe397d822a1a8de4c
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-05 04:26:20 -0700

    updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,10 @@
 
 
 def add_integer(a, b=98):
+    '''Return the integer addition of a and b only.
+    otherwise
+    raise type error
+    '''
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
