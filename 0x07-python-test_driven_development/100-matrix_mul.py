91aa1b8ede0eb738b7c4afafb5822da7d14990af
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-07 01:25:38 -0700

    matrix multiplication

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Defines a matrix multiplication function."""
+
+
+def matrix_mul(m_a, m_b):
+    """Multiply two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    Raises:
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied.
+    Returns:
+        A new matrix representing the multiplication of m_a by m_b.
+    """
+
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+
+    if not m_a or not m_b:
+        raise ValueError("m_a can't be empty or m_b can't be empty")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    
+    result = []
+    for i in range(len(m_a)):
+        row = []
+        for j in range(len(m_b[0])):
+            value = sum(m_a[i][k] * m_b[k][j] for k in range(len(m_a[i])))
+            row.append(value)
+        result.append(row)
+
+    return result
