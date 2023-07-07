e908a7ea8e90cf7ec0f623c599bb87ec667cf00a
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-07 04:08:10 -0700

    matrix multiplication with pandas

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
+'''Defining a matrix multiplication function using NumPy.'''
 import numpy as np
 
-def lazy_matrix_mul(m_a, m_b):
-        '''Return the multiplication of two matrices.
 
-    Args:
+def lazy_matrix_mul(m_a, m_b):
+    '''Return the multiplication of two matrices.
+
+    Arguments:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-        Raises:
-        ValueError: if m_a and m_b can not be multiplied.
-        Exception: if error occured during multiplication.
     '''
-    try:
-        result = np.matmul(m_a, m_b)
-        return result.tolist()
-    except ValueError as e:
-        raise ValueError("m_a and m_b can't be multiplied") from e
-    except Exception as e:
-        raise Exception("An error occurred during matrix multiplication") from e
+
+    return (np.matmul(m_a, m_b))
