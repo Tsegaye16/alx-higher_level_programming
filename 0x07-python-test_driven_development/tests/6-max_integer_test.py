73eb43a75df97333c05391784952a1ab17c73ce2
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-07 00:48:40 -0700

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-'''Unit-tests for maxim_integer([..]).'''
+'''Unit-tests for max_integer([..]).'''
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaximInteger(unittest.TestCase):
-    '''Define unittests for maxim_integer([..]).'''
+class TestMaxInteger(unittest.TestCase):
+    '''Define unittests for max_integer([..]).'''
 
     def test_ordered_list(self):
-    '''"Test an ordered list of integers.'''
+        '''Test an ordered list of integers.'''
         ordered = [1, 2, 3, 4]
         self.assertEqual(max_integer(ordered), 4)
 
     def test_unordered_list(self):
-        '''Test an unorderd list of integers.'''
+        '''Test an unordered list of integers.'''
         unordered = [1, 2, 4, 3]
         self.assertEqual(max_integer(unordered), 4)
 
@@ -45,17 +45,17 @@ class TestMaximInteger(unittest.TestCase):
 
     def test_string(self):
         '''Test a string.'''
-        string = 'Tsegaye'
-        self.assertEqual(max_integer(string), 's')
+        string = "Brennan"
+        self.assertEqual(max_integer(string), "r")
 
     def test_list_of_strings(self):
         '''Test a list of strings.'''
-        strings = ['Tsegaye', 'is', 'my', 'name']
-        self.assertEqual(max_integer(strings), 'name')
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
 
     def test_empty_string(self):
         '''Test an empty string.'''
-        self.assertEqual(max_integer(''), None)
+        self.assertEqual(max_integer(""), None)
 
 if __name__ == '__main__':
     unittest.main()
