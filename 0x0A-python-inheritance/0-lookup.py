5fc7d1fc216dd45999263c8dd193fb828e84221f
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-10 08:05:44 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining the function that returns
+    list of objects"""
+
+
+def lookup(obj):
+    """ method lookup(obje)
+        returns the list of objects
+    """
+    return dir(obj)
