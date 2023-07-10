c9d61be4f72dd928bcd292d14b1012bcc05dc30b
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-10 08:39:44 -0700

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining the method that check
+    weather object is instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """this method returns: 
+    True, if object is instance of class.
+    Otherwise, False."""
+    return isinstance(obj, a_class)
