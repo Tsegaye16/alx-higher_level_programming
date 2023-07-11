1590124c3a113c7ed3be3bd2b276383d2f75a7e9
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-11 11:46:12 -0700

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' function that returns the number of lines of a text file
+'''
+
+
+def write_file(filename="", text=""):
+    ''' function: number_of_lines
+    '''
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(text)
+            return len(text)
+    except IOError:
+        print("An error occurred while writing to the file.")
+        return 0
