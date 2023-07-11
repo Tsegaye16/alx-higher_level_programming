57a3729b4bcb84c46e29ffda2202d2563d73430d
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-11 13:15:43 -0700

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Defining the function that returns the dictionary
+    description with simple data structure
+'''
+
+
+def class_to_json(obj):
+    '''module class_to_json
+       returns builds a dictionary
+    '''
+    return obj.__dict__
