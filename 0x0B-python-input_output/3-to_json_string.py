caf56e74c9f9855c093917c6fd2163610648ae7d
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-11 12:10:24 -0700

    string to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' 
+    defining the function that displays json representation of string
+'''
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' module to_json_string
+     returns JSON representation
+    '''
+    return json.dumps(my_obj)
