db063711a2045db1690c051f0b160330b4b8459c
Tsegaye16 <abewatsegaye16@gmail.com>
2023-07-11 13:45:21 -0700

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''
-Defining module that append_after method.
-'''
+"""
+Module for append_after method.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
     '''Method for inserting text after search string.'''
     lines = []
-    with open(filename, "r", encoding="utf-8") as files:
-        lines = files.readlines()
-        i = 0
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        n = 0
         while n < len(lines):
             if search_string in lines[n]:
                 lines[n:n + 1] = [lines[n], new_string]
                 n += 1
             n += 1
-    with open(filename, "w", encoding="utf-8") as files:
-        files.writelines(lines)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
