6d5184ac857111dc383d4953146d464baee6af86
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-15 11:03:34 -0700

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,4 @@
-SELECT 'city', AVG('value') AS 'avg_temp' FROM 'temperature' WHERE
-'month' = 7 OR 'month' = 8 GROUP BY 'city' ORDER BY 'avg_temp' DESC
+-- goining two or more rows
+SELECT city, AVG(value) AS avg_temp FROM temperature WHERE
+month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC
 LIMIT 3
