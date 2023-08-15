24813ecb00cadf935fa369a81f04a823a6d35d23
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-15 11:13:24 -0700

    update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,2 @@
 -- Displaying the all rows that has not null value from second_table
-SELECT name, score FROM second_table
-WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT name, score FROM second_table WHERE name != '' ORDER BY score DESC;
