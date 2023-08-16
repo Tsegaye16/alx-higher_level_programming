f20b866068a44b72f38679630bab2bebb9f2875a
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 09:10:02 -0700

    update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creating the database hbtn_0d_usa with table states
 -- the table state hast two columns with some constraint
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-(id INT NOT NULL AUTO_INCEREMENT primary key, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT NULL AUTO_INCEREME PRIMARY KEY, name VARCHAR(256) NOT NULL);
