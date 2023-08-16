1c0158e410a8448b3ffd953956f7fff15ed6d8bb
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 09:30:29 -0700

    update

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- joining two or more table using left join
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_shos_genre.show_id
-ORDER BY tv_shows.title  ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
