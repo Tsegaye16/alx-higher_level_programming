57de308bfd8afd4d83184d8fbd92c8763e7396ae
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 09:04:01 -0700

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- show the privilages of the user on localhost server
+-- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
