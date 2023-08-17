9b18e42e0befab052e09e431686119ed989f7e7f
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 23:31:39 -0700

    more complex

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,5 @@
+-- Joining two or more tables is more complex
+SELECT name, SUM(rate) AS rating FROM tv_genres INNER JOIN
+tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN
+tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY name ORDER BY rating DESC;
