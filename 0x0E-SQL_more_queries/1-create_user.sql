c52fecf5ec07ca5e93ae8f66ff68370f898bd72b
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 22:43:26 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
--- crearing user with all privileges on the localhost server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY
-'user_0d_1_pwd' GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+-- creating the user_0d_1 user with all previleges
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
