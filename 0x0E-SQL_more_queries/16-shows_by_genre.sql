40e9f6e539edcbd59e38bc8d20134b7784362e9a
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-16 23:10:28 -0700

    complex

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- joining three tables using left join
+-- joining the entire table with null value to other
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN
+tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN
+tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY
+tv_shows.title, tv_genres.name;
