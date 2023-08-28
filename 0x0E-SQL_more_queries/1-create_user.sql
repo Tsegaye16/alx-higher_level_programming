cef37a59ff35cb9f18a5441e4562163701ee0ae1
Tsegaye16 <abewatsegaye16@gmail.com>
2023-08-27 23:34:28 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- creating the user_0d_1 user with all previleges
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
