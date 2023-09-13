180ae111c08f9a7e24185e10f23f0a32fa451e92
Tsegaye16 <abewatsegaye16@gmail.com>
2023-09-13 11:36:10 +0000

    hjfs

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.width; i++) {
+      let row = "";
+      for (let j = 0; j < this.height; j++) {
+        row = row + "X";
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
