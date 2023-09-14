54dfac29ae1951a70d898fb6d53aa4bcf56adcf9
Tsegaye16 <abewatsegaye16@gmail.com>
2023-09-14 00:58:01 +0000

    djksd

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,7 @@
 
 const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
+const Square = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
