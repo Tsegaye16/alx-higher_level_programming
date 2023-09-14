5e5913fe9fec9eb8ecde0837665f034029ce097c
Tsegaye16 <abewatsegaye16@gmail.com>
2023-09-14 00:31:47 +0000

    kjsd

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let i, j, result;
+    for (i = 0; i < this.width; i++) {
+      result = '';
+      for (j = 0; j < this.height; j++) {
+        result = result + 'X';
+      }
+      console.log(result);
+    }
+  }
+  rotate () {
+    let temp;
+    temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
