e0474c0f29fe87a6ac59f22fcc5b8ca40bed6c76
Tsegaye16 <abewatsegaye16@gmail.com>
2023-09-14 00:54:40 +0000

    djksd

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//square inherits from Rectangle
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
+
+module.exports = Square;
