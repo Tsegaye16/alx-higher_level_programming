57f416c3e9337671f0269f5d458eaeda60fa7068
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-02 14:20:13 -0700

    dkslfd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-
-'''the script that
-    fetches https://alx-intranet.hbtn.io/status.
-    uses urlib package
+'''The script that fetches https://alx-intranet.hbtn.io/status.
 '''
-import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
-try:
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        decoded_data = data.decode("utf-8")
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        result = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(data))
-        print("\t- content: {}".format(data)
-        print("\t- utf8 content: {}".format(decoded_data)
-
-except urllib.error.URLError as e:
-    print("Error:", e)
+        print("\t- type: {}".format(type(result)))
+        print("\t- content: {}".format(result))
+        print("\t- utf8 content: {}".format(result.decode('utf-8')))
