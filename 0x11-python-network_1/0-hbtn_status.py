4bdb21fa4db63c12f42b965b0083bf9814e72abe
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-02 14:49:46 -0700

    dhjsd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,9 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet'
-           ' .hbtn.io/status') as response:
-        result = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(result)))
-        print("\t- content: {}".format(result))
-        print("\t- utf8 content: {}".format(result.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
