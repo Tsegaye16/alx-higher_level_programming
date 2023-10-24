9425291bfb1f5a8dc91c2e52807d57b941c07efe
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-24 14:32:46 -0700

    jdksd

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,11 +4,11 @@
 const args = process.argv;
 let reqURL = args[2];
 let request = require('request');
-request(reqURL, function (err, res, content) {
-  if (err) {
-    console.log('error:', err);
+request(reqURL, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
   } else {
-    let todos = JSON.parse(content);
+    let todos = JSON.parse(body);
     let dash = {};
     for (let i = 0; i < todos.length; i++) {
       let status = (todos[i]['completed']);
